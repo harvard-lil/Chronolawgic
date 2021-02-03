5555ad565a270cfb10c54c3327198a7b3926ffbf
Andrew Silva <asilva@law.harvard.edu>
2021-02-02 23:18:41 -0500

    Added stub sample timeline to default route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -8,7 +8,32 @@ const bulkRoute = require('./bulk')
 const logRoute = require('./log')
 
 module.exports = () => {
-  router.get('/', (req, res) => res.send({ title: 'placeholder' }))
+  router.get('/', (req, res) => res.send(
+    {
+      title: 'Test Title',
+      introduction: 'This is a test timeline.',
+      created_by: 'Test Author String',
+      categories: [{
+        id: 1,
+        name: 'Test Topic',
+        color: '#99EEFF',
+        desc: 'This is a refreshing, cool blue test topic.'
+      }],
+      events: [
+        {
+          title: 'Test Event 1',
+          categories: [1],
+          text: 'This is the text for test event 1'
+        }
+      ],
+      app_meta: {
+        acl: ['user_id_1'],
+        created_by: 'user_id_1',
+        is_draft: false,
+        is_hidden: false
+      }
+    }
+  ))
 
   router.use('/timeline', timelineRoute())
   router.use('/user', userRoute())
